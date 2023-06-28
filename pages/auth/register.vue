07aad0bd55ea9d1d5d61301b8ca56eb1df6cf63a
Thomas Lindeker <thomas.lindeker@interieur.gouv.fr>
2023-06-28 11:43:52 +0200

    fix 2nd password

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,51 +1,60 @@
 <template>
-	<AppLayout>
-		<div class="space-y-4 w-fit mx-auto">
-			<div class="px-3 pb-4">
-				<h1 class="text-3xl">Hi, there</h1>
-				<p class="text-base">Signup to continue</p>
-			</div>
-			<form class="space-y-8 pb-4" spellcheck="false">
-				<TextInput label="Full Name" type="name" :text="credentials.email" />
-				<TextInput label="Email" type="email" :text="credentials.email" />
-				<TextInput
-					label="Password"
-					type="password"
-					v-model="credentials.password"
-				/>
-				<TextInput
-					label="Confirm Password"
-					type="password"
-					v-model="credentials.password"
-				/>
-			</form>
-			<FilledButton type="submit" class="w-full h-12" text="Sign up" />
-			<OutlinedButton
-				type="submit"
-				class="w-full h-12"
-				icon="logos:google-icon"
-				text="Sign up with Google"
-			/>
-			<div class="flex text-sm space-x-1"
-				><p>Already have an Account? </p>
-				<button
-					v-Ripple
-					class="cursor-pointer rounded-md text-colorPrimaryLight dark:text-colorPrimaryDark"
-					>Login</button
-				>
-			</div>
-		</div>
-	</AppLayout>
+    <AppLayout>
+        <div class="space-y-4 w-fit mx-auto">
+            <div class="px-3 pb-4">
+                <h1 class="text-3xl">Hi, there</h1>
+                <p class="text-base">Signup to continue</p>
+            </div>
+            <form class="space-y-8 pb-4" spellcheck="false">
+                <TextInput
+                    label="Full Name"
+                    type="name"
+                    :text="credentials.email"
+                />
+                <TextInput
+                    label="Email"
+                    type="email"
+                    :text="credentials.email"
+                />
+                <TextInput
+                    label="Password"
+                    type="password"
+                    v-model="credentials.password"
+                />
+                <TextInput
+                    label="Confirm Password"
+                    type="password"
+                    v-model="credentials.passwordRepeat"
+                />
+            </form>
+            <FilledButton type="submit" class="w-full h-12" text="Sign up" />
+            <OutlinedButton
+                type="submit"
+                class="w-full h-12"
+                icon="logos:google-icon"
+                text="Sign up with Google"
+            />
+            <div class="flex text-sm space-x-1">
+                <p>Already have an Account?</p>
+                <button
+                    v-Ripple
+                    class="cursor-pointer rounded-md text-colorPrimaryLight dark:text-colorPrimaryDark"
+                >
+                    Login
+                </button>
+            </div>
+        </div>
+    </AppLayout>
 </template>
 <script setup lang="ts">
-	const credentials = reactive({
-		firstName: "",
-		surname: "",
-		email: "",
-		password: "",
-		passwordRepeat: "",
-	});
-	definePageMeta({
-		title: "Register",
-	});
+const credentials = reactive({
+    firstName: "",
+    surname: "",
+    email: "",
+    password: "",
+    passwordRepeat: "",
+});
+definePageMeta({
+    title: "Register",
+});
 </script>
