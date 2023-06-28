3c75ca2b1fc53c3d7786be6bf2d7d46f997c2d0f
Thomas Lindeker <thomas.lindeker@interieur.gouv.fr>
2023-06-28 14:20:28 +0200

    auth not work already

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,52 +1,61 @@
 <template>
-	<AppLayout>
-		<div class="space-y-4 w-fit mx-auto">
-			<div class="px-3 pb-4">
-				<h1 class="text-3xl">Welcome back</h1>
-				<p class="text-base">Login to continue</p>
-			</div>
-			<form class="space-y-8 pb-4" spellcheck="false">
-				<TextInput
-					label="Email"
-					type="email"
-					placeholder="name@gmail.com"
-					:text="credentials.email"
-				/>
-				<TextInput
-					label="Password"
-					type="password"
-					placeholder="Password"
-					v-model="credentials.password"
-				/>
-			</form>
-			<FilledButton type="submit" class="w-full h-12" text="Sign in" />
-			<OutlinedButton
-				type="submit"
-				class="w-full h-12"
-				icon="logos:google-icon"
-				text="Sign in with Google"
-			/>
-			<div class="flex text-sm space-x-1"
-				><p>Don't have an account? </p>
-				<button
-					v-Ripple
-					class="cursor-pointer rounded-md text-colorPrimaryLight dark:text-colorPrimaryDark"
-					>Signup</button
-				></div
-			>
-		</div>
-	</AppLayout>
+    <AppLayout>
+        <div class="space-y-4 w-fit mx-auto">
+            <div class="px-3 pb-4">
+                <h1 class="text-3xl">Welcome back</h1>
+                <p class="text-base">Login to continue</p>
+            </div>
+            <form
+                class="space-y-8 pb-4"
+                spellcheck="false"
+                @submit.prevent="signIn(username, password)"
+            >
+                <TextInput
+                    label="Email"
+                    type="email"
+                    placeholder="name@gmail.com"
+                    :text="username"
+                />
+                <TextInput
+                    label="Password"
+                    type="password"
+                    placeholder="Password"
+                    v-model="password"
+                />
+            </form>
+            <FilledButton type="submit" class="w-full h-12" text="Sign in" />
+            <OutlinedButton
+                type="submit"
+                class="w-full h-12"
+                icon="logos:google-icon"
+                text="Sign in with Google"
+            />
+            <div class="flex text-sm space-x-1">
+                <p>Don't have an account?</p>
+                <button
+                    v-Ripple
+                    class="cursor-pointer rounded-md text-colorPrimaryLight dark:text-colorPrimaryDark"
+                >
+                    Signup
+                </button>
+            </div>
+        </div>
+    </AppLayout>
 </template>
 <script setup lang="ts">
-	const credentials = reactive({
-		email: "",
-		password: "",
-	});
+import { ref } from "vue";
+import { definePageMeta, useAuth } from "#imports";
 
-	const isTabletMode = computed(() => {
-		return window.innerWidth > 600;
-	});
-	definePageMeta({
-		title: "Login",
-	});
+const { signIn, token, data, status, lastRefreshedAt } = useAuth();
+
+const username = ref("");
+const password = ref("");
+
+definePageMeta({
+    title: "Login",
+    auth: {
+        unauthenticatedOnly: true,
+        navigateAuthenticatedTo: "/",
+    },
+});
 </script>
--- server/api/users.put.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export default defineEventHandler(async (event) => {
-    return {
-        data: "users.put.ts",
-    };
-});
